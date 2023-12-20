ea46163be5bd7a63b52dce2738a1f8253f12dac9
Faith <faithbrianna01@gmail.com>
2023-12-20 11:26:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+
+const main = document.querySelector('main#main');
+main.remove();
+
+// New header element with an id of 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+// content of the new header element
+const yourName = 'Faith Ogendi';
+newHeader.textContent = `${yourName} is the champion`;
+
+// Adds the new header element to the document
+const body = document.querySelector('body');
+// Set the nodeName property of the new element
+body.appendChild(newHeader);
